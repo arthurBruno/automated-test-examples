1fe2e02a83377f668e73f3427c8c3943cdc03444
Arthur Bruno <arthur.abbrr@gmail.com>
2020-11-02 17:21:09 -0300

    swapi url fix

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 
 const url = {
   URL_API_PETS: 'https://the4pets.herokuapp.com',
-  URL_API_SW: 'https://swapi.co/api',
+  URL_API_SW: 'https://swapi.dev/api',
   URL_API_JABUTI: 'https://augecorpjabuti.herokuapp.com/'
 };
 
