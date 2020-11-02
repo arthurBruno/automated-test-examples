3a090f93b7ab48bdc27952986bdcf6a94749927d
Arthur Bruno <arthur.abbrr@gmail.com>
2020-11-02 17:41:40 -0300

    Millennium Falcon films function and test

--- api/StarWars.js
+++ api/StarWars.js
@@ -1,18 +1,33 @@
 const axios = require('axios');
 const { URL_API_SW } = require('../secrets');
 
-const getPeople = async (nome) => {
-  const url = `${URL_API_SW}/people/?search=${nome}&format=json`;
-  const { data } = await axios.get(url);
+const getPeople = async (search) => {
+  const url = `${URL_API_SW}/people/`;
+  const params = {
+    search,
+    format: 'json'
+  };
+
+  const { data } = await axios.get(url, { params });
 
   return data.results.map(mapPeople);
 };
 
-const mapPeople = (item) => {
-  return {
-    name: item.name,
-    height: item.height
+const mapPeople = (item) => ({
+  name: item.name,
+  height: item.height
+});
+
+const getShipMovies = async (search) => {
+  const url = `${URL_API_SW}/starships/`;
+  const params = {
+    search,
+    format: 'json'
   };
+
+  const { data } = await axios.get(url, { params });
+
+  return data.results.map((item) => item.films);
 };
 
-module.exports = { getPeople };
+module.exports = { getPeople, getShipMovies };
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { getProducts } = require('../api/The4Pets');
-const { getPeople } = require('../api/StarWars');
+const { getPeople, getShipMovies } = require('../api/StarWars');
 
 
 describe('Testes The 4 Pets API', () => {
@@ -19,14 +19,27 @@ describe('Testes The 4 Pets API', () => {
 
 
 describe('Testes Star Wars API', () => {
-  it('Deve retornar o Luke Skywalker com o formato correto', async () => {
+  it('Retornar nome e altura do Luke Skywalker', async () => {
     const expected = {
       name: 'Luke Skywalker',
       height: '172'
     };
-    const baseName = 'Luke';
+    const search = 'Luke';
 
-    const result = await getPeople(baseName);
+    const result = await getPeople(search);
+
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it('Retornar filmes que a Millennium Falcon apareceu', async () => {
+    const expected = [
+      "http://swapi.dev/api/films/1/",
+      "http://swapi.dev/api/films/2/",
+      "http://swapi.dev/api/films/3/"
+    ];
+    const search = 'falcon';
+
+    const [result] = await getShipMovies(search);
 
     assert.deepStrictEqual(result, expected);
   });
