af94624dcafc1a1c820aacd254794138fb8b56f3
Arthur Bruno <arthur.abbrr@gmail.com>
2020-11-02 16:21:05 -0300

    secrets creation

---
+++ secrets.js
@@ -0,0 +1,8 @@
+
+const url = {
+  URL_API_PETS: 'https://the4pets.herokuapp.com',
+  URL_API_SW: 'https://swapi.co/api',
+  URL_API_JABUTI: 'https://augecorpjabuti.herokuapp.com/'
+};
+
+module.exports = { ...url };
